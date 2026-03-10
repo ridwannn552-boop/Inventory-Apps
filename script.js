7b31f3b171c69590aecde75e15b29dcde12c8026
ridwannn552-boop <ridwannn552@gmail.com>
2026-03-10 17:41:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ document.getElementById(pageId).classList.add("active");
 }
 
 document.getElementById("dashboard").classList.add("active");
+// DATA CONTOH
+
+let hari = ["Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"];
+
+let barangMasuk = [12,19,8,15,10,7];
+let barangKeluar = [10,14,6,11,9,5];
+
+
+// GRAFIK BARANG MASUK
+
+let ctx1 = document.getElementById('grafikMasuk');
+
+new Chart(ctx1,{
+type:'bar',
+data:{
+labels:hari,
+datasets:[{
+label:'Barang Masuk',
+data:barangMasuk
+}]
+}
+});
+
+
+// GRAFIK BARANG KELUAR
+
+let ctx2 = document.getElementById('grafikKeluar');
+
+new Chart(ctx2,{
+type:'line',
+data:{
+labels:hari,
+datasets:[{
+label:'Barang Keluar',
+data:barangKeluar
+}]
+}
+});
