3c469db0419f53657d53b59af2038c30cd300d37
ridwannn552-boop <ridwannn552@gmail.com>
2026-03-10 17:25:16 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function showPage(pageId){
+
+let pages=document.querySelectorAll(".page");
+
+pages.forEach(function(page){
+page.classList.remove("active");
+});
+
+document.getElementById(pageId).classList.add("active");
+
+}
+
+document.getElementById("dashboard").classList.add("active");
